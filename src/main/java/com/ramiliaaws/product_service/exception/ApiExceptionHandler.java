f3d661907d720f8465e16d58ebfab52873037a3b
Ramilia Marganova <rmarganova@ebsco.com>
2021-06-04 20:14:08 +0300

    Change exception handler

--- src/main/java/com/ramiliaaws/product_service/exception/ApiExceptionHandler.java
+++ src/main/java/com/ramiliaaws/product_service/exception/ApiExceptionHandler.java
@@ -1,25 +1,15 @@
 package com.ramiliaaws.product_service.exception;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
-
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
@@ -29,51 +19,57 @@ public class ApiExceptionHandler {
     public ResponseEntity<ErrorResponse> handle(ConstraintViolationException e) {
         ErrorResponse errors = new ErrorResponse();
         for (ConstraintViolation violation : e.getConstraintViolations()) {
-            ErrorItem error = ErrorItem.builder()
-                .code(violation.getMessageTemplate())
-                .message(violation.getMessage())
-                .status(BAD_REQUEST)
-                .build();
+            ErrorItem error = new ErrorItem();
+            error.setCode(violation.getMessageTemplate());
+            error.setMessage(violation.getMessage());
             errors.addError(error);
         }
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
+    @SuppressWarnings("rawtypes")
     @ExceptionHandler(ResourceNotFoundException.class)
-    @ResponseStatus(NOT_FOUND)
     public ResponseEntity<ErrorItem> handle(ResourceNotFoundException e) {
-        ErrorItem error = ErrorItem.builder()
-            .message("not found")
-            .status(NOT_FOUND)
-            .build();
+        ErrorItem error = new ErrorItem();
+        error.setMessage(e.getMessage());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorItem> handleAll(Exception e) {
-        ErrorItem error = ErrorItem.builder()
-            .message("internal error")
-            .status(INTERNAL_SERVER_ERROR)
-            .build();
-        return new ResponseEntity<>(error, new HttpHeaders(), error.getStatus());
-    }
-
-    @Builder
-    @Getter
-    @Setter
     public static class ErrorItem {
 
         @JsonInclude(JsonInclude.Include.NON_NULL)
         private String code;
+
         private String message;
-        private HttpStatus status;
+
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
     }
 
-    @Getter
-    @Setter
     public static class ErrorResponse {
 
         private List<ErrorItem> errors = new ArrayList<>();
+
+        public List<ErrorItem> getErrors() {
+            return errors;
+        }
+
+        public void setErrors(List<ErrorItem> errors) {
+            this.errors = errors;
+        }
 
         public void addError(ErrorItem error) {
             this.errors.add(error);
